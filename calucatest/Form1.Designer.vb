97797fb88f18337f82a77dbc8ac893cd55fd5782
zero22Dev1 <zero22dev@gmail.com>
2025-06-08 20:45:17 +0900

    プロジェクト ファイルを追加します。

---
+++ calucatest/Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button5 = New Button()
+        Button9 = New Button()
+        Button2 = New Button()
+        Button6 = New Button()
+        Button0 = New Button()
+        Button3 = New Button()
+        Button7 = New Button()
+        ButtonPlus = New Button()
+        Button4 = New Button()
+        Button8 = New Button()
+        ButtonEqual = New Button()
+        Label1 = New Label()
+        LabelResult = New Label()
+        LabelOperator = New Label()
+        Label2 = New Label()
+        Label5 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(41, 229)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(169, 98)
+        Button1.TabIndex = 0
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(238, 229)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(169, 98)
+        Button5.TabIndex = 0
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(432, 229)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(169, 98)
+        Button9.TabIndex = 0
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(41, 347)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(169, 98)
+        Button2.TabIndex = 0
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(238, 347)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(169, 98)
+        Button6.TabIndex = 0
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button0
+        ' 
+        Button0.Location = New Point(432, 347)
+        Button0.Name = "Button0"
+        Button0.Size = New Size(169, 98)
+        Button0.TabIndex = 0
+        Button0.Text = "0"
+        Button0.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(41, 451)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(169, 98)
+        Button3.TabIndex = 0
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(238, 451)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(169, 98)
+        Button7.TabIndex = 0
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonPlus
+        ' 
+        ButtonPlus.Location = New Point(432, 451)
+        ButtonPlus.Name = "ButtonPlus"
+        ButtonPlus.Size = New Size(169, 98)
+        ButtonPlus.TabIndex = 0
+        ButtonPlus.Text = "+"
+        ButtonPlus.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(41, 570)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(169, 98)
+        Button4.TabIndex = 0
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(238, 570)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(169, 98)
+        Button8.TabIndex = 0
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonEqual
+        ' 
+        ButtonEqual.Location = New Point(432, 570)
+        ButtonEqual.Name = "ButtonEqual"
+        ButtonEqual.Size = New Size(169, 98)
+        ButtonEqual.TabIndex = 0
+        ButtonEqual.Text = "="
+        ButtonEqual.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(52, 52)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(63, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Label1"
+        ' 
+        ' LabelResult
+        ' 
+        LabelResult.AutoSize = True
+        LabelResult.Location = New Point(461, 166)
+        LabelResult.Name = "LabelResult"
+        LabelResult.Size = New Size(63, 25)
+        LabelResult.TabIndex = 2
+        LabelResult.Text = "Label2"
+        ' 
+        ' LabelOperator
+        ' 
+        LabelOperator.AutoSize = True
+        LabelOperator.Location = New Point(214, 52)
+        LabelOperator.Name = "LabelOperator"
+        LabelOperator.Size = New Size(63, 25)
+        LabelOperator.TabIndex = 3
+        LabelOperator.Text = "Label3"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(432, 52)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(63, 25)
+        Label2.TabIndex = 3
+        Label2.Text = "Label3"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(75, 166)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(24, 25)
+        Label5.TabIndex = 3
+        Label5.Text = "="
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10.0F, 25.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(634, 728)
+        Controls.Add(Label5)
+        Controls.Add(Label2)
+        Controls.Add(LabelOperator)
+        Controls.Add(LabelResult)
+        Controls.Add(Label1)
+        Controls.Add(ButtonEqual)
+        Controls.Add(ButtonPlus)
+        Controls.Add(Button0)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button4)
+        Controls.Add(Button7)
+        Controls.Add(Button3)
+        Controls.Add(Button6)
+        Controls.Add(Button2)
+        Controls.Add(Button5)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents ButtonPlus As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents ButtonEqual As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LabelResult As Label
+    Friend WithEvents LabelOperator As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label5 As Label
+
+End Class
---
+++ calucatest/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+    Private input1 As String = ""
+    Private input2 As String = ""
+    Private operation As String = ""
+    Private isSecondInput As Boolean = False
+    Private resultDisplayed As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' 数字ボタン
+        AddHandler Button0.Click, AddressOf NumberButton_Click
+        AddHandler Button1.Click, AddressOf NumberButton_Click
+        AddHandler Button2.Click, AddressOf NumberButton_Click
+        AddHandler Button3.Click, AddressOf NumberButton_Click
+        AddHandler Button4.Click, AddressOf NumberButton_Click
+        AddHandler Button5.Click, AddressOf NumberButton_Click
+        AddHandler Button6.Click, AddressOf NumberButton_Click
+        AddHandler Button7.Click, AddressOf NumberButton_Click
+        AddHandler Button8.Click, AddressOf NumberButton_Click
+        AddHandler Button9.Click, AddressOf NumberButton_Click
+
+        ' 演算・イコールボタン
+        AddHandler ButtonPlus.Click, AddressOf PlusButton_Click
+        AddHandler ButtonEqual.Click, AddressOf EqualButton_Click
+    End Sub
+
+    ' 数字入力
+    Private Sub NumberButton_Click(sender As Object, e As EventArgs)
+        Dim btn As Button = CType(sender, Button)
+
+        If resultDisplayed Then
+            ' 結果表示後に入力されたらリセット
+            input1 = ""
+            input2 = ""
+            operation = ""
+            isSecondInput = False
+            resultDisplayed = False
+            Label1.Text = ""
+            Label2.Text = ""
+            LabelOperator.Text = ""
+            LabelResult.Text = ""
+        End If
+
+        If isSecondInput Then
+            input2 &= btn.Text
+            Label2.Text = input2
+        Else
+            input1 &= btn.Text
+            Label1.Text = input1
+        End If
+    End Sub
+
+    ' ＋ボタン処理
+    Private Sub PlusButton_Click(sender As Object, e As EventArgs)
+        If input1 <> "" And Not isSecondInput Then
+            operation = "+"
+            isSecondInput = True
+            LabelOperator.Text = "+"
+        End If
+    End Sub
+
+    ' ＝ボタン処理
+    Private Sub EqualButton_Click(sender As Object, e As EventArgs)
+        If input1 <> "" AndAlso input2 <> "" Then
+            Dim result As Double = 0
+
+            Select Case operation
+                Case "+"
+                    result = Val(input1) + Val(input2)
+                    ' 他の演算子も追加可能
+            End Select
+
+            ' 結果は右下のラベルに表示
+            LabelResult.Text = result.ToString()
+
+            ' 状態更新
+            input1 = result.ToString()
+            input2 = ""
+            operation = ""
+            isSecondInput = False
+            resultDisplayed = True
+        End If
+    End Sub
+End Class
